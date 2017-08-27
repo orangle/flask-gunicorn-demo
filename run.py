a7992c11126375b9747de954d09a15686797bee0
rn4ir <rn@rahul-nair.com>
2017-08-27 03:56:13 -0400

    added docstrings

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+"""
+This is 'run.py' - it calls the run() function for the instance
+of the Flask module that was created in the 'flaskr.py' file.
+"""
 
 from flaskr import application
 
