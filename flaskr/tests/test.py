c893e86111738c2488fe9ce817d2e40b3dfb9c9c
rn4ir <rn@rahul-nair.com>
2017-08-30 18:33:52 -0400

    added docstring comments for pylint tests

--- flaskr/tests/test.py
+++ flaskr/tests/test.py
@@ -1,38 +1,34 @@
 # project/test.py
- 
+"""
+This is 'test.py' - used to run a few unit tests on the code
+""" 
 import unittest
 from flaskr import flaskrapp
  
 class ProjectTests(unittest.TestCase):
- 
-    ############################
-    #### setup and teardown ####
-    ############################
- 
-    # executed prior to each test
+"""
+setup and teardown
+"""
     def setUp(self):
+    # executed prior to each test
         flaskrapp.config['TESTING'] = True
         flaskrapp.config['DEBUG'] = False
         self.flaskrapp = flaskrapp.test_client()
  
         self.assertEquals(flaskrapp.debug, False)
  
-    # executed after each test
     def tearDown(self):
+    # executed after each test
         pass
  
- 
-    ########################
-    #### helper methods ####
-    ########################
- 
- 
+    # helper methods
  
     ###############
     #### tests ####
     ###############
  
     def test_index_page(self):
+    # Test the index page text
         response = self.flaskrapp.get('/', follow_redirects=True)
         self.assertIn(b'Flaskr', response.data)
         self.assertIn(b'A simple Flask app.', response.data)
--- flaskr/views.py
+++ flaskr/views.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # File: flaskr/views.py
+"""
+This is 'views.py' - which uses decorators to generate views.
+"""
 
 # imports
-from flaskr import flaskrapp
+from flaskr import flaskrapp # pylint: disable=invalid-name
 from flask import render_template
 from flask import request
 
@@ -10,4 +13,7 @@ from flask import request
 @flaskrapp.route('/')
 @flaskrapp.route('/index')
 def index():
+    """
+    Function index(), to render the index file
+    """
     return render_template('index.html')
